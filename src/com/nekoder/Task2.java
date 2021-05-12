65e11d4b9c77cc66b1451512b830b9498233e0d3
dima <dimaannason@gmail.com>
2021-05-12 12:52:25 +0300

    Remove bugs from Task 2

--- src/com/nekoder/Task2.java
+++ src/com/nekoder/Task2.java
@@ -3,10 +3,8 @@ package com.nekoder;
 public class Task2 {
     public static void printFullAddress(String address) {
         System.out.println(
-                address.replaceAll("\\bAve\\.*\\b", "Avenue")
-                        .replaceAll("\\bStr?\\.*\\b", "Street")
-                        .replaceAll("Street\\.", "Street")
-                        .replaceAll("Avenue\\.", "Avenue")
+                address.replaceAll("\\bAve\\.?,", "Avenue,")
+                        .replaceAll("\\bStr?\\.?,", "Street,")
         );
     }
 }
